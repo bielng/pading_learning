d5f4dbf606a0b4a7febcf6027756638424de1e24
bielng <ngunartaban@gmail.com>
2025-12-25 23:32:15 -0600

    define the method turn_on

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ class Microwave:
     def __init__(self, brand: str, power_rating: str) -> None:
         self.brand = brand
         self.power_rating = power_rating
-    ...
+        self.turned_on: bool = False
+
+    def turn_on(self) -> None:
+        if self.turned_on:
+            print(f'Microwave ({self.brand}) is already turned on. ')
+
+    
 
 
 smeg: Microwave = Microwave(brand="Smeg", power_rating="B")
-print(smeg)
-print(smeg.brand)
-print(smeg.power_rating)
+# print(smeg)
+# print(smeg.brand)
+# print(smeg.power_rating)
 
 
-bosch: Microwave = Microwave(brand="Bosch", power_rating="A")
-print(bosch)
-print(bosch.brand)
-print(bosch.power_rating)
+# bosch: Microwave = Microwave(brand="Bosch", power_rating="A")
+# print(bosch)
+# print(bosch.brand)
+# print(bosch.power_rating)
