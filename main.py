f64a4a756334b55594abc8797bd50f46971a8bd6
bielng <ngunartaban@gmail.com>
2025-12-25 23:27:43 -0600

    define the type bosch

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ smeg: Microwave = Microwave(brand="Smeg", power_rating="B")
 print(smeg)
 print(smeg.brand)
 print(smeg.power_rating)
+
+
+bosch: Microwave = Microwave(brand="Bosch", power_rating="A")
+print(bosch)
+print(bosch.brand)
+print(bosch.power_rating)
